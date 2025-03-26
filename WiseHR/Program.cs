105d1ad12f056393ec1e78c1c85423081cad8d6a
KrishPugazh <krishpugazh10@gmail.com>
2025-03-26 11:27:33 +0530

    Authentication Done

--- WiseHR/Program.cs
+++ WiseHR/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using WiseHR.Services;
 using WiseHR;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Use HTTPS endpoint for the backend
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7021/") });
+
+builder.Services.AddMudServices();
+builder.Services.AddScoped<AuthService>();
 
 await builder.Build().RunAsync();
