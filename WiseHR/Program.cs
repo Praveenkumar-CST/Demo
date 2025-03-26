34f68f475b1e40205cc13655baab7c4022de870e
flowgirlcode <jayapriyaelumalai679@gmail.com>
2025-03-26 22:48:59 +0530

    role management page

--- WiseHR/Program.cs
+++ WiseHR/Program.cs
@@ -2,14 +2,14 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WiseHR;
 using MudBlazor.Services;
+using Supabase;
+using WiseHR.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
-
-
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7021/") });
 await builder.Build().RunAsync();
