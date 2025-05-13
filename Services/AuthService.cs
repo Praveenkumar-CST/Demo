910c15cff680b4e42d87de166c0b3207014d0409
flowgirlcode <jayapriyaelumalai679@gmail.com>
2025-05-13 16:12:58 +0530

    /load/success/UI/icons updates

--- Services/AuthService.cs
+++ Services/AuthService.cs
@@ -24,7 +24,7 @@ namespace WiseHR.Services
             //_baseUrl = configuration["ApiBaseUrl"] ?? "http://localhost:5243";
            _baseUrl = configuration["ApiBaseUrl"] ?? "https://localhost:7021";
             //_baseUrl = configuration["ApiBaseUrl"] ?? "http://172.210.14.62:5000/";
-         //_baseUrl = configuration["ApiBaseUrl"] ?? "https://wisehr-main-dce8e0bbg4f6djbs.eastus-01.azurewebsites.net/";
+          //_baseUrl = configuration["ApiBaseUrl"] ?? "https://wisehr-main-dce8e0bbg4f6djbs.eastus-01.azurewebsites.net/";
 
 
             _supabaseUrl = configuration["Supabase:Url"] ?? _supabaseUrl;
