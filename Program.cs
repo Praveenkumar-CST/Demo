857c67114eee9a8ebced656cffbad94838e9cbec
flowgirlcode <jayapriyaelumalai679@gmail.com>
2025-06-02 11:01:29 +0530

    urlupdate

--- Program.cs
+++ Program.cs
@@ -53,6 +53,7 @@ builder.Services.AddMemoryCache();
 builder.Services.AddScoped<SearchService>();
 
 builder.Services.AddScoped<IUserService, UserService>();
+
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7021") });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://wisehr-main-dce8e0bbg4f6djbs.eastus-01.azurewebsites.net") });
 await builder.Build().RunAsync();
