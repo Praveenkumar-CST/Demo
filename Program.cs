4889b83fa94e624690e60f406eb4b8d0b2cb6da9
flowgirlcode <jayapriyaelumalai679@gmail.com>
2025-06-19 22:59:13 +0530

    OrgUI and ReportModelUpdate

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ using FingerFrontend.AdminAttendanceViewModel;
 using Microsoft.Extensions.Caching.Memory;
 using Syncfusion.Blazor;
 using WiseHR.Models.NewFolder;
+using Syncfusion.Licensing;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -68,13 +69,14 @@ builder.Services.AddScoped<LeaveManagementService>();
 
 builder.Services.AddSyncfusionBlazor();
 
+
 builder.Services.AddScoped<IUserService, UserService>();
 var apiBaseUrl = "https://wisehr-main-dce8e0bbg4f6djbs.eastus-01.azurewebsites.net";
 //var apiBaseUrl = "https://localhost:7021/";
 
 // Register ChatService
 builder.Services.AddScoped<IChatService, ChatService>();
-
+SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NNaF1cWWhPYVFwWmFZfVtgdl9GaFZVRWYuP1ZhSXxWdkNiUH9ccHxRR2JZWEF9XUs=");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 builder.Services.AddSingleton(new ApiConfig { BaseUrl = apiBaseUrl });
