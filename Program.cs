1c2782c7ac4fdf5a9f4e19bcbc696d30afeb65cd
tejasm-189 <143952904+tejasm-189@users.noreply.github.com>
2025-06-20 12:09:15 +0530

    backend url

--- Program.cs
+++ Program.cs
@@ -64,8 +64,8 @@ builder.Services.AddScoped<LeaveManagementService>();
 builder.Services.AddSyncfusionBlazor();
 
 builder.Services.AddScoped<IUserService, UserService>();
-//var apiBaseUrl = "https://wisehr-main-dce8e0bbg4f6djbs.eastus-01.azurewebsites.net";
-var apiBaseUrl = "https://localhost:7021/";
+var apiBaseUrl = "https://wisehr-main-dce8e0bbg4f6djbs.eastus-01.azurewebsites.net";
+//var apiBaseUrl = "https://localhost:7021/";
 
 // Register ChatService
 builder.Services.AddScoped<IChatService, ChatService>();
