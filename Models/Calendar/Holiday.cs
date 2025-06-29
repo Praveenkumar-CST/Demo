e04f4657d90ec6501fd5013e57b2077f8a0262cc
Praveenkumar-CST <praveenkumar165516@gmail.com>
2025-06-29 13:03:30 +0530

    123

--- Models/Calendar/Holiday.cs
+++ Models/Calendar/Holiday.cs
@@ -2,8 +2,8 @@ namespace list.Models;
 public class Events
  {
         public string? Date { get; set; }
-        public string? Description { get; set; }
-        public string? HolidayType { get; set; }
+        public string? t; }
+        public s]okdayType { get; set; }
         public string? Location { get; set; }
         public string? Day { get; set; }
     
